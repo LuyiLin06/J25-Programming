17a61ca8d096ef5ad3ee7c87ad1070ae7fa3e9fd
LuyiLin06 <145320400+LuyiLin06@users.noreply.github.com>
2024-02-05 08:47:55 +0100

    Create TriangleComputation.java

---
+++ Java/Codes/TriangleComputation.java
@@ -0,0 +1,15 @@
+public class Main { 
+   public static void main(String[] args) {
+      double base, sidea, sideb, height, area, perimeter;
+      base = 1.2;
+      sidea = 2.4;
+      sideb = 3.0;
+      height = 2.5;
+      area = (base * height)/2;
+      perimeter = base + sidea + sideb;
+      System.out.print("The area is ");
+      System.out.println(area);
+      System.out.print("The perimeter is ");
+      System.out.println(perimeter);
+   }
+}
