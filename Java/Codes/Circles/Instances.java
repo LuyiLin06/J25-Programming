a56a61f9c5df5c383ace69db3f75b95e0942944a
LuyiLin06 <145320400+LuyiLin06@users.noreply.github.com>
2024-03-05 17:35:29 +0100

    Update and rename Instances to Instances.java

---
+++ Java/Codes/Circles/Instances.java
@@ -0,0 +1,5 @@
+Circle circle1 = new Circle();
+Circle c2 = new Circle(5.0);
+Circle c3 = new Circle(5.0);
+Circle colinRobinson = new Circle(-2.0, "greenish");
+Circle circle5 = new Circle(1.0, "blue");
