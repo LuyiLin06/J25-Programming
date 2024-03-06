8b6127619a54121afcd72c49b665ecbc44d024c5
LuyiLin06 <145320400+LuyiLin06@users.noreply.github.com>
2024-03-06 22:33:18 +0100

    Create BookDetails.java

---
+++ Java/Codes/BookDetails.java
@@ -0,0 +1,29 @@
+public class BookDetails {
+  String title, author, isbn;
+  int numberOfPages;
+  boolean isKorean;
+
+  public Book(){
+    this.title = "Book";
+    this.author = "Author X";
+    this.numberOfPages = 0;
+    this.inKorean = false;
+    this.ibn = "XXXXXXXXXXXXX"
+  }
+  
+  public Book (String title){
+    this.title = title;
+    this.author = "Author X";
+    this.numberOfPages = 0;
+    this.inKorean = false;
+    this.ibn = "XXXXXXXXXXXXX";
+  }
+
+  public Book (String title, int numberOfPages){
+    this.title = title;
+    this.author = "Author X";
+    this.numberOfPages = 0;
+    this.inKorean = false;
+    this.ibn = "XXXXXXXXXXXXX";
+  }
+}
