800023d4cdd99f6080cf6eb6e79ab7433b161acb
LuyiLin06 <145320400+LuyiLin06@users.noreply.github.com>
2024-03-11 08:56:49 +0100

    Create objectvariables.java

---
+++ Java/Codes/objectvariables.java
@@ -0,0 +1,21 @@
+//Coche Familiar​
+String model​
+String plateNumber​
+float price​
+int seats​
+bool hibrid​
+bool warranty
+
+//Coche Formula1
+String team​
+String pilot​
+double weight​
+int potency​
+bool sponsorship
+
+// Coche Electrico
+String model​
+String plateNumber​
+float price​
+int batteryCapacity​
+bool autonomous
